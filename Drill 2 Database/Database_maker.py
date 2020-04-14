f92981f925a29c9d4d109332a9b772609c5cdc2d
MarkFilip <4markfilipkowski@gmail.com>
2020-04-14 16:19:25 -0600

    Add Drill 2
    add database and Pyton file to create and query the database

---
+++ Drill 2 Database/Database_maker.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+fileList = ('information.docx','Hello.txt','myImage.png', \
+            'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
+
+conn = sqlite3.connect('test.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_files ( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+        file_name TEXT \
+        )")
+    conn.commit()
+conn.close
+
+with conn:
+    cur = conn.cursor()
+    textList=[]
+    for i in fileList:
+        if i.endswith('.txt') == True:
+            textList.append(i)
+    for t in textList:    
+        cur.execute("INSERT INTO tbl_files(file_name) VALUES (?)", \
+                        [t]) 
+    conn.commit()
+conn.close
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("SELECT file_name FROM tbl_files")
+    varFiles = cur.fetchall()
+    for item in varFiles:
+        msg = "File Name: {}".format(item[0])
+        print(msg)
+        
+
+
+
+
+
+
+
+
+
+        
