2f7f5d67e83866bd4b1829a4c8f267f51e83a0a8
MarkFilip <4markfilipkowski@gmail.com>
2020-09-30 12:05:53 -0600

    Update SpaceInvaders
    Add functions for leveling up and winning. Implement OOP

--- SpaceInvaders/main.py
+++ SpaceInvaders/main.py
@@ -2,6 +2,9 @@ import pygame
 import random
 import math
 from pygame import mixer
+import sys
+
+# ---------------------- Game setup ----------------------
 
 # Initialize  pygame
 pygame.init()
@@ -24,49 +27,69 @@ pygame.display.set_icon(icon)
 # Game State options are start, ready, level-up
 game_state = "start"
 
-# Player
-playerImg = pygame.image.load('circlelogo-white.png')
-playerX = 370
-playerY = 480
-playerX_change = 0
-
 # Level
 level_value = 1
 font = pygame.font.Font('freesansbold.ttf', 32)
 levelX = 10
 levelY = 45
 
-# Enemy
+# enemy set up
 enemy_icons = ['chrome.png', 'css-3.png', 'javascript.png', 'sql-server.png', 'python.png', 'hashtag.png']
-enemyImg = []
-enemyX = []
-enemyY = []
-enemyX_change = []
-enemyY_change = []
 num_of_enemies = 6
-enemy_speed = 0.75 * level_value
+enemy_speed = 0.5 * level_value
+enemies_per_level = 3
+winning_score = enemies_per_level * num_of_enemies
+# ---------------------- Classes ----------------------
 
-for i in range(num_of_enemies):
-    enemyImg.append(pygame.image.load(enemy_icons[level_value-1]))
-    enemyX.append(random.randint(0, 736))
-    enemyY.append(random.randint(50, 150))
-    enemyX_change.append(2)
-    enemyY_change.append(40)
+
+# Player
+class Player:
+    Img = pygame.image.load('circlelogo-white.png')
+    X = 370
+    Y = 480
+    X_change = 0
+
+    def show_player(self, x, y):
+        screen.blit(self.Img, (x, y))  # blit means to draw (Icon, tuple with coordinates)
+
+
+# Enemy
+class Enemy:
+    def __init__(self):
+        self.Img = pygame.image.load(enemy_icons[level_value - 1])
+        self.X = random.randint(0, 736)
+        self.Y = random.randint(50, 150)
+        self.X_change = 2
+        self.Y_change = 40
+
+    def show_enemy(self, x, y):
+        screen.blit(self.Img, (x, y))
+
 
 # Bullet
-bulletImg = pygame.image.load('checked.png')
-bulletX = 0
-bulletY = 480
-bulletX_change = 0
-bulletY_change = 10
-bullet_state = "ready"  # ready means you can't see the bullet on the screen, fire means it is moving
+class Bullet:
+    Img = pygame.image.load('checked.png')
+    X = 0
+    Y = 480
+    X_change = 0
+    Y_change = 10
+    state = "ready"  # ready means you can't see the bullet on the screen, fire means it is moving
 
+    def fire(self, x, y):
+        self.state = "fire"
+        screen.blit(self.Img, (x + 16, y + 10))  # The coordinates are changed to center the bullet
+
+    def reload(self):
+        self.state = "ready"
+        self.Y = 480
+
+
+# ---------------------- Text Creation and Display----------------------
 # Score
 score_value = 0
 font = pygame.font.Font('freesansbold.ttf', 32)
 scoreX = 10
 scoreY = 10
-
 
 # Start text
 start_font = pygame.font.Font('freesansbold.ttf', 48)
@@ -92,11 +115,6 @@ def show_level(x, y):
     screen.blit(level, (x, y))
 
 
-def game_over_text():
-    over_text = over_font.render("GAME OVER", True, (0, 0, 0))
-    screen.blit(over_text, (230, 230))
-
-
 def start_game_text():
     start_text = start_font.render("Press ENTER", True, (0, 0, 0))
     screen.blit(start_text, (220, 230))
@@ -107,36 +125,115 @@ def level_up_text():
     screen.blit(level_text, (260, 230))
 
 
+def game_over_text():
+    over_text = over_font.render("GAME OVER", True, (0, 0, 0))
+    screen.blit(over_text, (230, 230))
+
+
 def win_game_text():
     win_text = win_font.render("YOU WIN!", True, (0, 0, 0))
     screen.blit(win_text, (260, 230))
 
 
-def player(x, y):
-    screen.blit(playerImg, (x, y))  # blit means to draw (Icon, tuple with coordinates)
-
-
-def enemy(x, y, i):
-    screen.blit(enemyImg[i], (x, y))
-
-
-def fire_bullet(x, y):
-    global bullet_state
-    bullet_state = "fire"
-    screen.blit(bulletImg, (x + 16, y + 10))  # The coordinates are changed to center the bullet
-
-
+# ---------------------- Game mechanic functions ----------------------
 # collision detecting function
-def isCollision(enemyX, enemyY, bulletX, bulletY):
+def is_collision(enemy_x, enemy_y, bullet_x, bullet_y):
     # Distance formula. If distance less than 27 then it is a collision
-    distance = math.sqrt(math.pow(enemyX - bulletX, 2) + math.pow(enemyY - bulletY, 2))
+    distance = math.sqrt(math.pow(enemy_x - bullet_x, 2) + math.pow(enemy_y - bullet_y, 2))
     if distance < 27:
         return True
     else:
         return False
 
 
-# Game loop.
+# function that creates an array of enemies
+def enemy_creation(number_of_enemies):
+    list_of_enemies = []
+    for i in range(num_of_enemies):
+        list_of_enemies.append(Enemy())
+    return list_of_enemies
+
+
+# function that brings up the start menu
+def start(state):
+    starting = True
+    while starting:
+        start_game_text()  # display start text
+        for event in pygame.event.get():
+            # Allow the user to quit
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            # Have the user press enter to begin the game
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    state = 'ready'
+                    return state
+        # Draw the player, score and level while waiting
+        player.show_player(player.X, player.Y)
+        show_score(scoreX, scoreY)
+        show_level(levelX, levelY)
+        pygame.display.update()
+
+
+# function that brings up the level-up menu
+def level_up(level):
+    leveling = True
+    while leveling:
+        level_up_text()  # display level-up text
+        for event in pygame.event.get():
+            # Allow the user to quit
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            # Have the user press enter to start the next level
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    # update level and enemy speed
+                    level += 1
+                    speed = 0.5 * level
+                    state = 'ready'
+                    return state, level, speed
+        # Draw the player, score and level while waiting
+        player.show_player(player.X, player.Y)
+        show_score(scoreX, scoreY)
+        show_level(levelX, levelY)
+        pygame.display.update()
+
+
+# function that ends the game and allows for a reset
+def win():
+    winning = True
+    while winning:
+        win_game_text() # display level-up text
+        # Allow the user to quit
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            # Have the user press enter to play again
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    state = 'ready'
+                    # reset level, score, and enemy speed
+                    level = 1
+                    score = 0
+                    speed = 0.5 * level_value
+                    return state, level, score, speed
+        # Draw the player, score and level while waiting
+        player.show_player(player.X, player.Y)
+        show_score(scoreX, scoreY)
+        show_level(levelX, levelY)
+        pygame.display.update()
+
+
+# ---------------------- Initialization ----------------------
+player = Player()
+bullet = Bullet()
+enemies = enemy_creation(num_of_enemies)
+
+
+# ---------------------- Game loop ----------------------
 running = True
 while running:
     # Background color within rgb tuple
@@ -146,39 +243,19 @@ while running:
 
     # start game instructions
     if game_state == "start":
-        start_game_text()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    game_state = 'ready'
+        game_state = start(game_state)
+
     # Level up
     elif game_state == "level-up":
-        level_up_text()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    level_value += 1
-                    enemy_speed = 0.5 * level_value
-                    for i in range(num_of_enemies):
-                        enemyImg[i] = pygame.image.load(enemy_icons[level_value-1])
-                    game_state = 'ready'
+        game_state, level_value, enemy_speed = level_up(level_value)
+        enemies = enemy_creation(num_of_enemies)
+
+    # Winning the game and resetting
     elif game_state == 'win':
-        win_game_text()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    score_value = 0
-                    level_value = 1
-                    enemy_speed = 0.5 * level_value
-                    for i in range(num_of_enemies):
-                        enemyImg[i] = pygame.image.load(enemy_icons[0])
-                    game_state = 'ready'
+        game_state, level_value, score_value, enemy_speed = win()
+        enemies = enemy_creation(num_of_enemies)
+
+    # During gameplay
     else:
         # Events are any user inputs
         for event in pygame.event.get():
@@ -189,81 +266,80 @@ while running:
             # if a keystroke is pressed, check whether it is left or right
             if event.type == pygame.KEYDOWN:  # Checks if a key is pushed
                 if event.key == pygame.K_LEFT:
-                    playerX_change = -5  # Moves the X coordinate left
+                    player.X_change = -5  # Moves the X coordinate left
                 if event.key == pygame.K_RIGHT:
-                    playerX_change = 5  # Moves the X coordinate right
+                    player.X_change = 5  # Moves the X coordinate right
                 if event.key == pygame.K_SPACE:
-                    if bullet_state == "ready":  # If there isn't already a bullet on the screen
+                    if bullet.state == "ready":  # If there isn't already a bullet on the screen
                         # bullet_sound = mixer.Sound('laser.wav')  # Sets the sound of the bullet
                         # bullet_sound.play()  # Plays it once
-                        bulletX = playerX  # Get's the X coordinate of the ship to give to the bullet
-                        fire_bullet(bulletX, bulletY)  # Fire the bullet
+                        bullet.X = player.X  # Get's the X coordinate of the ship to give to the bullet
+                        bullet.fire(bullet.X, bullet.Y)  # Fire the bullet
             if event.type == pygame.KEYUP:  # Checks if a keystroke has been released
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    playerX_change = 0  # Doesn't move the X coordinate left
+                    player.X_change = 0  # Doesn't move the X coordinate left
 
         # player movement
-        playerX += playerX_change
+        player.X += player.X_change
 
         # checks boundaries of the player
-        if playerX <= 0:
-            playerX = 0
-        elif playerX >= 736:
-            playerX = 736
+        if player.X <= 0:
+            player.X = 0
+        elif player.X >= 736:
+            player.X = 736
 
         # Enemy movement
         for i in range(num_of_enemies):
 
-            if enemyY[i] > 440:
+            if enemies[i].Y > 440:
                 for j in range(num_of_enemies):
-                    enemyY[j] = 2000
+                    enemies[j].Y = 2000
                 game_over_text()
                 break
-
-            enemyX[i] += enemyX_change[i]
+            # Update change in X. Add enemy speed so respawned enemies move at correct speed
+            enemies[i].X += enemies[i].X_change + enemy_speed
 
             # checks boundaries of the enemy
-            if enemyX[i] <= 0:
-                enemyX_change[i] = 2 + enemy_speed
-                enemyY[i] += enemyY_change[i]
-            elif enemyX[i] >= 736:
-                enemyX_change[i] = -2 - enemy_speed
-                enemyY[i] += enemyY_change[i]
+            if enemies[i].X <= 0:
+                enemies[i].X_change = 2
+                enemies[i].Y += enemies[i].Y_change
+            elif enemies[i].X >= 736:
+                enemies[i].X_change = - (2 + 2 * enemy_speed)
+                # enemy speed has to be subtracted twice to compensate for addition earlier
+                enemies[i].Y += enemies[i].Y_change
 
             # Collision
-            collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+            collision = is_collision(enemies[i].X, enemies[i].Y, bullet.X, bullet.Y)
             if collision:
                 # explosion_sound = mixer.Sound('explosion.wav')  # Sets the sound of the explosion
                 # explosion_sound.play()  # Plays it once
                 # Resets the bullet
-                bulletY = 480
-                bullet_state = "ready"
+                bullet.reload()
                 # Update score
                 score_value += 1
-                if score_value > 17:
+                if score_value == winning_score:
                     game_state = "win"
-                elif score_value % 3 == 0:
+                elif score_value % enemies_per_level == 0:
                     game_state = "level-up"
                 # Reset enemy
-                enemyX[i] = random.randint(0, 736)
-                enemyY[i] = random.randint(50, 150)
+                enemies[i].X = random.randint(0, 736)
+                enemies[i].Y = random.randint(50, 150)
 
             # Draw enemy
-            enemy(enemyX[i], enemyY[i], i)
+            enemies[i].show_enemy(enemies[i].X, enemies[i].Y)
 
         # Bullet movement
         # When it reaches the end, make the bullet ready for another fire
-        if bulletY <= 0:
-            bulletY = 480
-            bullet_state = "ready"
+        if bullet.Y <= 0:
+            bullet.reload()
 
         # Plot the bullet when it is fired. Keep changing the Y value
-        if bullet_state == "fire":
-            fire_bullet(bulletX, bulletY)
-            bulletY -= bulletY_change
+        if bullet.state == "fire":
+            bullet.fire(bullet.X, bullet.Y)
+            bullet.Y -= bullet.Y_change
 
     # Draw the player
-    player(playerX, playerY)
+    player.show_player(player.X, player.Y)
     # Draw the score and level
     show_score(scoreX, scoreY)
     show_level(levelX, levelY)
